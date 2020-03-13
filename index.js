62266f3549334a95190d727b50ca51c76c3ffb10
zeurjr <59857096+elizeurjr@users.noreply.github.com>
2020-03-13 00:33:44 -0300

    Teste de objeto finalizado

---
+++ index.js
@@ -0,0 +1,9 @@
+const nome = "Supremos"
+const cor = "Roxo"
+const foco = "Programação"
+const endereco = { 
+  rua: "Rua Guilherme Gembala",
+  numero: 260
+}
+
+console.log (`A empresa ${nome} está localizada em ${endereco.rua}, ${endereco.numero}`)
